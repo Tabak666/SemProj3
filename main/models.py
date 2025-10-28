95c417545c1af6c5b5c93fba92c83ef03e2426ab
kikosolovic <kisolovic@gmail.com>
2025-10-28 17:39:46 +0100

    added user table with test values

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    gender = models.CharField(max_length=100)
+    height = models.IntegerField()
+    activity_level = models.IntegerField()
+    role = models.CharField(max_length=20)
+    table_id = models.IntegerField(db_default=None, null=True)
+
