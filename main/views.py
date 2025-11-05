0edd588585d28a4357acf116223d1fd126ea86b3
Tabak666 <roman.tabacek2005@gmail.com>
2025-11-05 18:56:26 +0100

    Admin approval

--- main/views.py
+++ main/views.py
@@ -17,9 +17,14 @@ def login_view(request):
 
             try:
                 user = Users.objects.get(username=username)
-                if check_password(password, user.password): #comparing password if they match
+                if not user.approved:
+                    messages.error(request, 'Your account wasnt approved')
+                    return redirect('login')
+                
+                if check_password(password, user.password):
                     request.session['user_id'] = user.id
                     request.session['username'] = user.username
+                    request.session['role'] = user.role
                     return redirect('index')
                 else:
                     messages.error(request, 'Invalid password')
@@ -36,12 +41,44 @@ def register_view(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save(commit=False)
+            user.approved = False
+            user.save()
             return redirect('login')
     else:
         form = RegistrationForm()
 
     return render(request, 'register.html', {'form': form})
+
+def approvals_view(request):
+    if not request.session.get('user_id'):
+        return redirect('login')
+    if request.session.get('role') != 'admin':
+        return redirect('index')
+
+    pending_users = Users.objects.filter(approved=False)
+
+    if request.method == 'POST':
+        user_id = request.POST.get('user_id')
+        action = request.POST.get('action')
+        try:
+            user = Users.objects.get(id=user_id)
+
+            if action == 'approve':
+                user.approved = True
+                user.save()
+                messages.success(request, f'{user.username} has been approved.')
+
+            elif action == 'decline':
+                user.delete()
+                messages.warning(request, f'{user.username} has been declined and removed.')
+
+        except Users.DoesNotExist:
+            messages.error(request, "User not Found")
+        return redirect('approvals')
+    
+    return render(request, 'approvals.html', {'pending_users': pending_users})
+
 
 def dashboard_view(request):
     if not request.session.get('user_id'):
